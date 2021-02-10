aeefe9eca61b0342216115f2c63cc0d8a83dc490
oliinykroman <oliinykroman@gmail.com>
2021-02-10 12:02:44 +0200

    upload files

---
+++ plugins/vue-global.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import FormatHtml from '@/globalComponents/FormatHtml.vue';
+import Hint from '@/globalComponents/Hint.vue';
+Vue.component('FormatHtml', FormatHtml);
+Vue.component('Hint', Hint);
