6afd22727112f94f6fb6aeb5204a0c927b5d4edb
oliinykroman <oliinykroman@gmail.com>
2021-02-11 19:57:55 +0200

    update specialization section

--- components/Specialization.vue
+++ components/Specialization.vue
@@ -1,9 +1,12 @@
 <template>
   <section class="specialization">
     <div class="container">
-      <!-- <div class="hidden1">{{ content }}</div> -->
+      <!-- <div class="hidden1">{{ content.services }}</div> -->
       <div class="inner-wrap">
-        <h2 class="section-title">
+        <h2
+          v-if="content.service_title && content.service_title[0].text"
+          class="section-title"
+        >
           {{ content.service_title[0].text }}
         </h2>
 
@@ -13,14 +16,23 @@
             :key="item.id"
             class="services-item"
           >
-            <h5 class="item-title">
-              {{ item.service_name[0].text }}
-            </h5>
-            <p>
-              {{ item.service_text[0].text }}
-            </p>
-            <div class="icon-holder">
-              <img :src="item.service_image.url" alt="" />
+            <div class="inner-item">
+              <h5
+                v-if="item.service_name && item.service_name[0].text"
+                class="item-title"
+              >
+                {{ item.service_name[0].text }}
+              </h5>
+              <p v-if="item.service_text && item.service_text[0].text">
+                {{ item.service_text[0].text }}
+              </p>
+              <div class="icon-holder">
+                <img
+                  v-if="item.service_image && item.service_image.url"
+                  :src="item.service_image.url"
+                  alt=""
+                />
+              </div>
             </div>
           </li>
         </ul>
