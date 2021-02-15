fa00fada0ca240293479e01ec9d6f0b2c23d5b6f
Анастасия Николенко <anastasianikolenko@MacBook-Pro-Anastasia.local>
2021-02-15 17:36:51 +0200

    update

--- components/Form.vue
+++ components/Form.vue
@@ -5,6 +5,28 @@
         <h2 class="section-title">
           {{ content.form_title[0].text }}
         </h2>
+
+        <div>
+          <!-- <form class="layout-form">
+            <div
+              class="form-group"
+              :class="{ error: validation.hasError('email') }"
+            >
+              <div class="label">* Email</div>
+              <div class="content">
+                <input v-model="email" type="text" class="form-control" />
+              </div>
+              <div class="message">{{ validation.firstError('email') }}</div>
+            </div>
+            <div class="form-group">
+              <div class="actions">
+                <button type="button" class="btn btn-primary" @click="submit">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form> -->
+        </div>
       </div>
     </div>
   </section>
