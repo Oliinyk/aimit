bd8b6ffa0e31fc40f5acd4dc5310c6b50e2a460a
Анастасия Николенко <anastasianikolenko@MacBook-Pro-Anastasia.local>
2021-02-16 15:30:13 +0200

    homepage

--- components/About.vue
+++ components/About.vue
@@ -9,24 +9,36 @@
           {{ content.about_title[0].text }}
         </h2>
 
-        <h3 class="title">
-          We have strong experience in
-          <span>web</span>, <span>ui/ux</span>,
-          <span>e-commerce</span>
-        </h3>
+        <div class="about-row">
+          <div class="col-text">
+            <h3 class="title">
+              We have strong experience in
+              <span>web</span>, <span>ui/ux</span>,
+              <span>e-commerce</span>
+            </h3>
 
-        <p v-if="content.about_text && content.about_text[0].text">
-          {{ content.about_text[0].text }}
-        </p>
+            <p v-if="content.about_text && content.about_text[0].text">
+              {{ content.about_text[0].text }}
+            </p>
 
-        <div class="btn-holder">
-          <a
-            v-if="content.about_btn && content.about_btn[0].text"
-            href="#"
-            class="btn"
-          >
-            {{ content.about_btn[0].text }}
-          </a>
+            <div class="btn-holder">
+              <a
+                v-if="content.about_btn && content.about_btn[0].text"
+                href="#"
+                class="btn"
+              >
+                {{ content.about_btn[0].text }}
+              </a>
+            </div>
+          </div>
+
+          <div class="col-img">
+            <img
+              v-if="content.about_image && content.about_image.url"
+              :src="content.about_image.url"
+              :alt="content.about_image.alt"
+            />
+          </div>
         </div>
       </div>
     </div>
